373b9d073c19da07d7b20f4a8c4acc6c98f95e23
GYUJEONG HWANG <octave08b@gmail.com>
2018-02-11 00:03:58 +0900

    ejs와 router 적용
    ejs 엔진을 등록하고 각 페이지들을 위한 router를 추가했습니다

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile)
+
+app.use(express.static('public'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+const router = require('./router/main')(app)
